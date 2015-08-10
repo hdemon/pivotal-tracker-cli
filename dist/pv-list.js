d4b384810f7c026333fa569da3f176c5a4c63ce1
Masami Yonehara <zeitdiebe@gmail.com>
2015-08-11 00:21:41 +0900

    refactor

--- dist/pv-list.js
+++ dist/pv-list.js
@@ -1,24 +1,23 @@
 #!/usr/bin/env node --harmony --harmony_arrow_functions --harmony_modules
-
-
-"use strict";
+'use strict';
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _commander = require('commander');
+
+var _commander2 = _interopRequireDefault(_commander);
 
 var _story = require('./story');
 
 var _story2 = _interopRequireDefault(_story);
 
-var program = require('commander');
+_commander2['default'].option('-p, --project-id [projectId]', 'project id').option('-t, --token [token]', 'token').parse(process.argv);
 
-program.option('-p, --project-id [projectId]', 'project id').option('-t, --token [token]', 'token').parse(process.argv);
-
-var PROJECT_ID = program.projectId;
-var TOKEN = program.token;
-
-_story2['default'].set({ projectId: PROJECT_ID, token: TOKEN });
+_story2['default'].set({ projectId: _commander2['default'].projectId, token: _commander2['default'].token });
 _story2['default'].getList().then(function (stories) {
-  console.log(stories);
+  stories.forEach(function (story) {
+    console.log(story.name);
+  });
 })['catch'](function (err) {
   console.error(colors.red("error: " + err.data.general_problem));
 });
