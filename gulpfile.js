6fcd8029e250abcdcf68ba7ee04d032d225a2af8
Masami Yonehara <zeitdiebe@gmail.com>
2015-08-11 10:15:58 +0900

    separate query logic

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var watch = require('gulp-watch');
+var rename = require('gulp-rename');
+var chmod = require('gulp-chmod');
 
 gulp.task('build', function () {
   return gulp.src('src/*')
     .pipe(babel())
+    .pipe(rename(function (path) {
+      path.extname = ""
+    }))
+    .pipe(chmod(755))
     .pipe(gulp.dest('dist'));
 });
 
@@ -12,5 +18,9 @@ gulp.task('watch', function () {
   return gulp.src('src/*')
     .pipe(watch('src/*'))
     .pipe(babel())
+    .pipe(rename(function (path) {
+      path.extname = ""
+    }))
+    .pipe(chmod(755))
     .pipe(gulp.dest('dist'));
 });
