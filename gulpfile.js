a99d6650f8b1714093fad537b1bf0d4edfeb422e
Masami Yonehara <zeitdiebe@gmail.com>
2015-08-17 00:30:46 +0900

    refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var watch = require('gulp-watch');
-var rename = require('gulp-rename');
-var chmod = require('gulp-chmod');
+var gulp = require('gulp')
+var babel = require('gulp-babel')
+var rename = require('gulp-rename')
+var chmod = require('gulp-chmod')
 
 gulp.task('build', function () {
   return gulp.src('src/*')
@@ -11,16 +10,9 @@ gulp.task('build', function () {
       path.extname = ""
     }))
     .pipe(chmod(755))
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(gulp.dest('dist'))
+})
 
 gulp.task('watch', function () {
-  return gulp.src('src/*')
-    .pipe(watch('src/*'))
-    .pipe(babel())
-    .pipe(rename(function (path) {
-      path.extname = ""
-    }))
-    .pipe(chmod(755))
-    .pipe(gulp.dest('dist'));
-});
+  gulp.watch('src/*', ['build'])
+})
